41348874a482b9ba81123da8cbade24ffb6fa941
Sagar Pawar <pawarsgr18@gmail.com>
2017-05-27 15:27:41 +0530

    swapping program

---
+++ 6. swaping.py
@@ -0,0 +1,7 @@
+a=float(input("Enter Number 1:"))
+b=float(input("Enter Number 2:"))
+print("value of a is %f ad value of b is %f" %(a,b))
+temp=a
+a=b
+b=a
+print("value of a is %f ad value of b is %f" %(a,b))
